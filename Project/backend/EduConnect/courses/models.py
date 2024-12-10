80286797c4e1ddb004bf37209f85540fa3baabbf
TrahimchikFedor <trahimchik.fedor@gmail.com>
2024-12-10 17:15:25 +0300

    FIXIT

--- Project/backend/EduConnect/courses/models.py
+++ Project/backend/EduConnect/courses/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
+    
+class Course(models.Model):
+    name = models.CharField(max_length=255 , db_index=True)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 class Lecture(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.PROTECT, null = True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     # course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lectures')  # Связь с курсом
@@ -12,11 +19,3 @@ class Lecture(models.Model):
 
     def __str__(self):
         return self.title
-    
-class Course(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    lectures = models.ManyToManyField(Lecture)
-
-    def __str__(self):
-        return self.name
