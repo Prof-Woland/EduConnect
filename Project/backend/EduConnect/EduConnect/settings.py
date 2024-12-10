964503318a41cff49b2d07b69965bcd2f9a6ac0f
TrahimchikFedor <trahimchik.fedor@gmail.com>
2024-12-10 12:27:29 +0300

    f

--- Project/backend/EduConnect/EduConnect/settings.py
+++ Project/backend/EduConnect/EduConnect/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'EduConnect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [settings.BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
