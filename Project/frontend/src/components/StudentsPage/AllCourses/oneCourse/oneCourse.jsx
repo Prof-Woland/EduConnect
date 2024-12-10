194896a34eab503e80a1b84f63284532175d430a
Prof-Woland <sorokonikita08@gmail.com>
2024-12-10 15:35:35 +0300

    CourseStyle

--- Project/frontend/src/components/StudentsPage/AllCourses/oneCourse/oneCourse.jsx
+++ Project/frontend/src/components/StudentsPage/AllCourses/oneCourse/oneCourse.jsx
@@ -2,9 +2,13 @@ import "./oneCourse.css"
 export default function OneCourse({title,description}){
     return(
         <div className="oneCourseDiv">
-            <p className="title">{title}</p>
-            <p className="description">{description}</p>
-            <button className="addCourses">Добавить курс</button>
+            <div>
+                <p className="title">{title}</p>
+                <p className="description">{description}</p>
+            </div>
+            <div>
+                <button className="addCourses">Добавить курс</button>
+            </div>
         </div>
     )
 }
--- Project/frontend/src/components/StudentsPage/StudentsPage.jsx
+++ Project/frontend/src/components/StudentsPage/StudentsPage.jsx
@@ -7,19 +7,21 @@ import YourCourses from "./YourCourses/YourCourses"
 export default function StudentsPage({}){
 return(
     <>
+    <div className="YoupPage">
     <div className="YoupPageDiv">
-        <h1 className="YoupPageDivH1">Ваша страница:</h1>
+        {/* <h1 className="YoupPageDivH1">Ваша страница:</h1> */}
     </div>
     <div className="YourPageDescription">
         <div>
             <h2>Доступные курсы:</h2>
             <AllCourses />
         </div>
-        <div>
+        {/* <div>
             <h2>Ваши курсы:</h2>
             <YourCourses  />
-        </div>
+        </div> */}
         
+    </div>
     </div>
     
     </>
